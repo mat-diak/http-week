be177b84c59f25a22bfd144653413ed70d96cf74
exit <exit>
2022-01-04 16:13:40 +0000

    creating new routes and adding reloader

--- Gemfile
+++ Gemfile
@@ -6,7 +6,9 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-ruby '3.0.3'
+ruby '3.1.0'
 
 gem 'webrick'
 gem 'sinatra'
+
+gem 'sinatra-contrib'
--- lib/app.rb
+++ lib/app.rb
@@ -1,2 +1,8 @@
 require 'sinatra'
+require 'sinatra/reloader' #if development?
 
+
+get '/' do "hello world" end
+get '/secret' do "hello! ruby" end
+get '/route_a' do "hello route_a" end
+  
