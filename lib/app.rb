8e489e8c71b953dcf0d7499d3f2aabe4eb172cd0
exit <exit>
2022-01-04 17:20:04 +0000

    Erb and Css_step10

--- lib/app.rb
+++ lib/app.rb
@@ -5,4 +5,4 @@ require 'sinatra/reloader' #if development?
 get '/' do "hello world" end
 get '/secret' do "hello! ruby" end
 get '/route_a' do "hello route_a" end
-get '/cat' do '<style> img {border: dashed red;} </style><img src="https://i.imgur.com/jFaSxym.png"><style>' end
+get '/cat' do erb(:index) end
