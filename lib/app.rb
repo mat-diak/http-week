8729fbacdc2c6a911d8f2c38ed9de35e617007cb
mikitaro-27 <matt.diak@gmail.com>
2022-01-05 14:39:44 +0000

    exctrat code from .erb file to app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -5,4 +5,7 @@ require 'sinatra/reloader' #if development?
 get '/' do "hello world" end
 get '/secret' do "hello! ruby" end
 get '/route_a' do "hello route_a" end
-get '/cat' do erb(:index) end
+get '/cat' do 
+  @name = %w(Amigo Misty Almond).sample
+  erb(:index)
+end
