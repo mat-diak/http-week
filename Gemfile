ee2b258bf9eb6b041ce3e9780a82ac14e4cca9b9
mikitaro-27 <matt.diak@gmail.com>
2022-01-06 14:12:48 +0000

    finish Capybara step

--- Gemfile
+++ Gemfile
@@ -4,11 +4,10 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
-
 ruby '3.1.0'
 
 gem 'webrick'
 gem 'sinatra'
-
+gem 'capybara'
+gem 'selenium-webdriver'
 gem 'sinatra-contrib'
--- lib/app.rb
+++ lib/app.rb
@@ -2,10 +2,28 @@ require 'sinatra'
 require 'sinatra/reloader' #if development?
 
 
-get '/' do "hello world" end
-get '/secret' do "hello! ruby" end
-get '/route_a' do "hello route_a" end
-get '/cat' do 
+get '/' do 
+  "hello world" 
+end
+
+get '/secret' do
+   "hello! ruby" 
+end
+
+get '/route_a' do
+   "hello route_a" 
+end
+
+get '/random_cat' do 
   @name = %w(Amigo Misty Almond).sample
   erb(:index)
+end
+
+get '/cat_form' do
+  erb :cat_form
+end
+
+post '/named_cat' do
+  @name = params[:name]
+  erb :index
 end
